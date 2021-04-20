dc8f2b94afe7f81cbd81d6d312bb92760d0d9826
crestini1844738 <crestini.1844738@studenti.uniroma1.it>
2021-04-21 00:29:32 +0200

    commit

--- public/php/login.php
+++ public/php/login.php
@@ -55,5 +55,82 @@
             
             return $this->body;  
         }  
-    } */ 
+    }  
+
+    session_start();
+
+    $options['host'] = "localhost";   
+    $options['port'] = 5984; 
+
+    //METTERE LE PROPRIE CREDENZIALI DI COUCH DB
+     //HO ABUSIVAMENTE MESSO LE CREDENZIALI MIE DI COUCH DB 
+    $userDB="admin";
+    $passDB="admin";
+
+    $username= $_POST["Username"];
+    $password= $_POST["Password"];
+
+    if (isset($_SESSION['session_id'])) {
+        header('Location: dashboard.php');
+        exit;
+    }
+
+    if (empty($username) || empty($password)) {
+        $msg = 'Inserisci username e password %s';
+    } else {
+        //query a couch db, password di quell utente
+        $user['password']="Password1234";
+    
+
+    if (!$user || password_verify($password, $user['password']) === false) {
+        $msg = 'Credenziali utente errate %s';
+    } else {
+        session_regenerate_id();
+        $_SESSION['session_id'] = session_id();
+        $_SESSION['session_user'] = $user['username'];
+        
+        header('Location: dashboard.php');
+        exit;
+    }
+}
+
+printf($msg, '<a href="../login.html">torna indietro</a>');
+}
+*/
+
+
+
+
+
+session_start();
+
+if (isset($_SESSION['session_id'])) {
+    header('Location: dashboard.php');
+    exit;
+}
+
+if (isset($_POST['Login'])) {
+    $username = $_POST['username'] ?? '';
+    $password = $_POST['password'] ?? '';
+    
+    if (empty($username) || empty($password)) {
+        $msg = 'Inserisci username e password %s';
+    } else {
+        $user["Username"]="Username1234";
+        $user['Password']="Password1234";
+        
+        if (!$user || password_verify($password, $user['Password']) === false) {
+            $msg = 'Credenziali utente errate %s';
+        } else {
+            session_regenerate_id();
+            $_SESSION['session_id'] = session_id();
+            $_SESSION['session_user'] = $user['Username'];
+            
+            header('Location: dashboard.php');
+            exit;
+        }
+    }
+    
+    printf($msg, '<a href="../login.html">torna indietro</a>');
+}
 ?>
