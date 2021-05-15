410f70beff3317b4eb832b00c4beabaa287a7be1
martinatombolini <tombolini.1844737@studenti.uniroma1.it>
2021-05-15 11:23:08 +0200

    prova descrizione

--- public/JavascriptPersonal/profilo.js
+++ public/JavascriptPersonal/profilo.js
@@ -68,7 +68,11 @@ function personalArea(utente)
             var img;
             if(tuttiicorsi[j].image == 'loaded') img = "<img src='../img/courseImgs/"+author+"_"+courseName+".png' id='profilocorso'></div>";
             else img="<img src='../img/courseImgs/default_image.png' id='profilocorso'></div>";
-            var description="Some text about the course..";
+            //var description="Some text about the course..";
+            var description=tuttiicorsi[j].desc;
+            /*var description;
+            if(tuttiicorsi[j].desc === undefined) description="";
+            else description=tuttiicorsi[j].desc;*/
             corsiProfilo=corsiProfilo+"<div class='card' ><div class='row g-0'>"+
             "<div class='col-md-3' id='imgcorso'>"+img+
             "<div class='col-md-9'><div class='card-body'><h1>"+courseName+"</h1>"+
